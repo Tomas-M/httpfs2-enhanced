8faa9d82b325698654362095e8b39fc6949ed0a5
TomasM <tomas@slax.org>
2017-10-28 03:56:46 -0400

    added buildroot config files for manual usage if needed

---
+++ buildroot/httpfs.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# httpfs
+#
+################################################################################
+
+HTTPFS_VERSION = master
+HTTPFS_SOURCE = master.tar.gz
+HTTPFS_SITE = https://github.com/Tomas-M/httpfs2-enhanced/archive
+HTTPFS_LICENSE = GPLv2+, LGPLv2+
+HTTPFS_DEPENDENCIES = host-pkgconf libfuse
+
+
+define HTTPFS_BUILD_CMDS
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define LIBFOO_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/httpfs2 $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
